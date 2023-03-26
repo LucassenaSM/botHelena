089519db5b4f4255702a92893e5a1161b05300ac
Lucas Sena <lucassenasilvamachado@gmail.com>
2023-03-26 12:29:11 -0300

    Base 2 Protegida

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const puppeteer = require("puppeteer");
+require('dotenv').config();
+
+const username = process.env.TW_USER;
+const senha = process.env.TW_SENHA;
+
 
 (async function abrirLinks() {
   const browser = await puppeteer.launch({ headless: true });
@@ -6,10 +11,11 @@ const puppeteer = require("puppeteer");
 
   await page.goto("https://twitter.com/i/flow/login");
   await page.waitForTimeout(1000)
-  await page.type('[class="css-1dbjc4n r-1roi411 r-z2wwpe r-rs99b7 r-18u37iz"]', "HelenaAcervo");
+  await page.type('[class="css-1dbjc4n r-1roi411 r-z2wwpe r-rs99b7 r-18u37iz"]', `${username}`);
+  await page.waitForTimeout(500)
  await page.click('[class="css-18t94o4 css-1dbjc4n r-sdzlij r-1phboty r-rs99b7 r-ywje51 r-usiww2 r-2yi16 r-1qi8awa r-1ny4l3l r-ymttw5 r-o7ynqc r-6416eg r-lrvibr r-13qz1uu"]');
  await page.waitForTimeout(1000)
-  await page.type('[type="password"]', "Lucas007")
+ await page.type('[type="password"]', `${senha}`)
   await page.waitForTimeout(1000)
   await page.click('[class="css-1dbjc4n r-pw2am6"]');
   await page.waitForTimeout(1000)
